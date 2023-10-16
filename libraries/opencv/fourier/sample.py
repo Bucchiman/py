53de3afe066bc3b656660cee905a87ac4aa6e586
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-10-16 18:03:05 +0900

    add sample codes

--- libraries/opencv/fourier/sample.py
+++ libraries/opencv/fourier/sample.py
@@ -5,7 +5,7 @@
 # Author:       8ucchiman
 # CreatedDate:  2023-07-27 13:18:37
 # LastModified: 2023-02-18 14:28:37 +0900
-# Reference:    8ucchiman.jp
+# Reference:    https://docs.opencv.org/4.8.0/de/dbc/tutorial_py_fourier_transform.html
 # Description:  ---
 #
 
@@ -26,6 +26,53 @@ def main():
     plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
     plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
     plt.show()
+
+    rows, cols = img.shape
+    crow,ccol = rows//2 , cols//2
+    fshift[crow-30:crow+31, ccol-30:ccol+31] = 0
+    f_ishift = np.fft.ifftshift(fshift)
+    img_back = np.fft.ifft2(f_ishift)
+    img_back = np.real(img_back)
+    plt.subplot(131),plt.imshow(img, cmap = 'gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(132),plt.imshow(img_back, cmap = 'gray')
+    plt.title('Image after HPF'), plt.xticks([]), plt.yticks([])
+    plt.subplot(133),plt.imshow(img_back)
+    plt.title('Result in JET'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
+def main02():
+    import numpy as np
+    import cv2 as cv
+    from matplotlib import pyplot as plt
+    img = cv.imread('messi5.jpg', cv.IMREAD_GRAYSCALE)
+    assert img is not None, "file could not be read, check with os.path.exists()"
+    dft = cv.dft(np.float32(img), flags=cv.DFT_COMPLEX_OUTPUT)
+    dft_shift = np.fft.fftshift(dft)
+    magnitude_spectrum = 20*np.log(cv.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
+    plt.subplot(121), plt.imshow(img, cmap='gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(magnitude_spectrum, cmap='gray')
+    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+    rows, cols = img.shape
+    crow, ccol = rows/2, cols/2
+    # create a mask first, center square is 1, remaining all zeros
+    mask = np.zeros((rows, cols, 2), np.uint8)
+    mask[crow-30:crow+30, ccol-30:ccol+30] = 1
+    # apply mask and inverse DFT
+    fshift = dft_shift*mask
+    f_ishift = np.fft.ifftshift(fshift)
+    img_back = cv.idft(f_ishift)
+    img_back = cv.magnitude(img_back[:, :, 0], img_back[:, :, 1])
+    plt.subplot(121), plt.imshow(img, cmap='gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(img_back, cmap='gray')
+    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
 
 
 if __name__ == "__main__":
