dd31a022677b49e00d4fec32ba3460de594d6f3b
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-11-15 19:39:09 +0900

    update tips

--- pocket/tips.py
+++ pocket/tips.py
@@ -174,3 +174,36 @@ def matplotlib_fig2image():
 # Reference:        https://docs.opencv.org/3.1.0/db/d5b/tutorial_py_mouse_handling.html
 
 # -------------------------------------------
+def mouse_event_matplotlib():
+    # Reference:        https://matplotlib.org/stable/gallery/event_handling/coords_demo.html
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    from matplotlib.backend_bases import MouseButton
+
+    t = np.arange(0.0, 1.0, 0.01)
+    s = np.sin(2 * np.pi * t)
+    fig, ax = plt.subplots()
+    ax.plot(t, s)
+
+
+    def on_move(event):
+        if event.inaxes:
+            print(f'data coords {event.xdata} {event.ydata},',
+                  f'pixel coords {event.x} {event.y}')
+
+
+    def on_click(event):
+        if event.button is MouseButton.LEFT:
+            print('disconnecting callback')
+            plt.disconnect(binding_id)
+
+
+    binding_id = plt.connect('motion_notify_event', on_move)
+    plt.connect('button_press_event', on_click)
+
+    plt.show()
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+# -------------------------------------------
